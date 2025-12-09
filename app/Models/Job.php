f84daa8c21b73877731c56cb6e682906bb2a05dd
Pavlina <pavlina.malinkova@email.cz>
2025-12-09 15:45:44 +0100

    in the middle of the episode about Eloquent

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -2,39 +2,10 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Arr;
 
-class Job
+class Job extends Model
 {
-  public static function all(): array
-  {
-    return [
-      [
-        'id' => 1,
-        'title' => 'Teacher',
-        'salary' => 7500
-      ],
-      [
-        'id' => 2,
-        'title' => 'Programmer',
-        'salary' => 8500
-      ],
-      [
-        'id' => 3,
-        'title' => 'Proctologist',
-        'salary' => 9500
-      ]
-    ];
-  }
-
-  public static function find(int $id): array
-  {
-    $job = Arr::first(self::all(), fn($job) => $job['id'] === $id);
-
-    if (!$job) {
-      abort(404);
-    }
-
-    return $job;
-  }
+  protected $table = 'job_listings';
 }
