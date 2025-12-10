5b547158fcb7758c3f432315bb3763d721d1adcd
Pavlina <pavlina.malinkova@email.cz>
2025-12-10 10:51:03 +0100

    finished the block about Eloquent

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,24 +2,28 @@
 
 namespace Database\Seeders;
 
+use App\Models\Job;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-    use WithoutModelEvents;
+  use WithoutModelEvents;
 
-    /**
-     * Seed the application's database.
-     */
-    public function run(): void
-    {
-        // User::factory(10)->create();
+  /**
+   * Seed the application's database.
+   */
+  public function run(): void
+  {
+    // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
-    }
+    User::factory()->create([
+      'first_name' => 'John',
+      'last_name' => 'Doe',
+      'email' => 'test@example.com',
+    ]);
+
+    Job::factory(200)->create();
+  }
 }
--- resources/views/jobs.blade.php
+++ resources/views/jobs.blade.php
@@ -1,12 +1,20 @@
 <x-layout>
   <x-slot:heading>Jobs listing</x-slot>
-    <h1 class="text-3xl">Jobs page</h1>
 
 
-    <ul>
+
+    <div class="space-y-4">
       @foreach ($jobs as $job)
-        <li><a href="/jobs/{{ $job['id'] }}">{{ $job['title'] }} / {{ $job['salary'] }}</a></li>
+        <a class="block px-4 py-6 border border-gray-300 rounded-lg" href="/jobs/{{ $job['id'] }}">
+          <div class="font-bold text-blue-500">{{ $job->employer->name }}</div>
+          <div><strong>{{ $job['title'] }}</strong> / {{ $job['salary'] }}</div>
+        </a>
       @endforeach
-    </ul>
+
+      <div>{{ $jobs->links() }}</div>
+
+    </div>
+
+
 
 </x-layout>
